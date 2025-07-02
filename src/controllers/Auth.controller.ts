58dcd4b74bef2e769cb13ae55c6f791e9557cb7c
lkast <lkastiman@gmail.com>
2025-07-02 18:36:48 -0500

    revert

--- src/controllers/Auth.controller.ts
+++ src/controllers/Auth.controller.ts
@@ -34,7 +34,7 @@ export class AuthController {
 
 		res.cookie('auth-token', token, {
 			httpOnly: true,
-			secure: false, // HTTPS in production
+			secure: process.env.NODE_ENV === 'production' || process.env.RAILWAY_ENVIRONMENT_NAME, // HTTPS in production
 			sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
 			maxAge: parseInt(process.env.COOKIE_EXPIRATION_DURATION) * 1000 , //TODO:
 		});
