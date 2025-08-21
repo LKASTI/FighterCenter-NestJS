b73ed84cf11df1bef57d338649397ed87733f7ca
lkast <lkastiman@gmail.com>
2025-08-21 18:59:57 -0500

    Added initial swagger config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from "@nestjs/common";
 import * as cookieParser from "cookie-parser";
 import "reflect-metadata";
 import { BasicAuth } from "./authentication/basicAuth";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -52,6 +53,14 @@ async function bootstrap() {
         const basicAuthMiddleware = new BasicAuth();
         app.use(basicAuthMiddleware.use.bind(basicAuthMiddleware));
     }
+
+
+    const config = new DocumentBuilder()
+        .setTitle('FighterCenter Swagger API')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
 
     await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
 }
