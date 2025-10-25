dbf9a133681184054662fd26e2e0648582b75b9e
lkast <lkastiman@gmail.com>
2025-10-25 14:01:01 -0500

    Added cviper

--- src/common/constants/sf6-characters.ts
+++ src/common/constants/sf6-characters.ts
@@ -1,5 +1,5 @@
 export const SF6_CHARACTERS = [
-    'Akuma', 'Blanka', 'Cammy', 'Chun-Li', 'Dee Jay', 'Dhalsim',
+    'Akuma', 'Blanka', 'Cammy', 'Chun-Li', 'C. Viper', 'Dee Jay', 'Dhalsim',
     'E. Honda', 'Ed', 'Elena', 'Guile', 'Juri', 'Ken', 'Kimberly',
     'Lily', 'Luke', 'M. Bison', 'Mai', 'Manon', 'Marisa', 'Rashid',
     'Ryu', 'Sagat', 'Terry', 'Zangief', 'JP', 'A.K.I.', 'Random'
